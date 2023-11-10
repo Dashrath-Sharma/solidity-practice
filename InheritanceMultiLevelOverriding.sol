4a9e65f5f1622f74ffb478a3d42e1de41b6d73e1
Dashrath Sharma <123117106+Dashrath-Sharma@users.noreply.github.com>
2023-11-11 02:25:36 +0530

    Multi Level Overriding.
    Inheritance - Multi Level Overriding

---
+++ InheritanceMultiLevelOverriding.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Inheritance Concept along with overriding methods.
+
+contract Parent{
+    // virtual Keyword allows children contracts to access and modify this function in their declaration;
+    function toBeOverridden() public pure virtual returns(string memory){
+        return "In Parent";
+    }
+}
+
+contract Child is Parent{
+    // override Keyword in conjuction with virtual keyword allows children to change the definition of a function declared inside parent
+    function toBeOverridden() public pure virtual override returns(string memory){
+        return "In Child";
+    }
+}
+
+contract GrandChild is Child{
+    function toBeOverridden() public pure virtual override returns(string memory){
+        return "In GrandChild";
+    }
+}
+
+contract GreatGrandChild is Child{
+    function toBeOverridden() public pure override returns(string memory){
+        return "In GreatGrandChild";
+    }
+}
