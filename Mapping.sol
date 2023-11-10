425e676403e252599f154aaf9c7f1048331a37e5
Dashrath Sharma <123117106+Dashrath-Sharma@users.noreply.github.com>
2023-11-10 12:48:05 +0530

    Use of mapping( Solidity Object )
    Mapping is a solidity object that holds values as { Key : Value } pairs.

---
+++ Mapping.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Structure of a person who will denate some amount;
+struct donner {
+    string name;
+    string add;
+    uint256 donation;
+}
+
+contract Mapping {
+    // Mapping can have keys as all the value type variables, Enums, and Contracts;
+    mapping(address => donner) public donatiee;
+
+    // Function that takes all the details that donner struct requires and sets it into donatiee ( mapping );
+    // msg.sender gets the address of the person who is using the smart contract.
+    function setDonatiee(
+        string memory _name,
+        string memory _add,
+        uint256 _donation
+    ) public {
+        donatiee[msg.sender] = donner(
+            _name,
+            _add,
+            donatiee[msg.sender].donation + _donation
+        );
+    }
+}
